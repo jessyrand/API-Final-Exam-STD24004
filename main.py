4d2c88fcdfd3dab2c7a6c26a781f6e9785e420ca
jessyrand <henintsoarandriam@gmail.com>
2025-09-03 11:59:34 +0300

    feat: add route to search car by id

--- main.py
+++ main.py
@@ -40,3 +40,9 @@ def serialize_cars():
 def get_cars():
     return JSONResponse(content=serialize_cars(), status_code=200, media_type="application/json")
 
+@app.get("/cars/{id}")
+def get_car(id: str):
+    for car in cars_stored:
+        if car.id == id:
+            return JSONResponse(content=car.model_dump(), status_code=200, media_type="application/json")
+    return JSONResponse(content="Car not found", status_code=404, media_type="text/plain")
